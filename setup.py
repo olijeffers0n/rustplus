942ccda053b31ca4271f686495fa4adfe02c263e
Ollie <69084614+olijeffers0n@users.noreply.github.com>
2021-05-03 10:47:55 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
       project_urls={
         "Issue tracker": "https://github.com/olijeffers0n/rustplus/issues",
       },
-      version="1.1.1",
+      version="1.2",
       include_package_data=True,
       packages = ['rustplus', 'rustplus.api', 'rustplus.api.icons', 'rustplus.exceptions'],
       license='MIT',
