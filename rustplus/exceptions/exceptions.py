2b77b0066bd8b5243c597731b14bd80aa84c6c53
Ollie <69084614+olijeffers0n@users.noreply.github.com>
2021-06-16 10:13:42 +0100

    Add ServerNotResponsiveError

--- rustplus/exceptions/exceptions.py
+++ rustplus/exceptions/exceptions.py
@@ -7,6 +7,11 @@ class ClientError(Error):
     """Raised when the client details are not valid"""
     pass
 
+
 class ImageError(Error):
     """Raised when the Image Returned is not valid"""
+    pass
+
+class ServerNotResponsiveError(Error):
+    """Raised when the target Server is not online / Unavailable"""
     pass
