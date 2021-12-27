2219cf7a25cb3afbf4f94e8a2fda82eb2372398d
olijeffers0n <69084614+olijeffers0n@users.noreply.github.com>
2021-12-27 18:06:00 +0000

    Automatic Version on setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,36 @@
 from setuptools import setup, find_packages
 
-readme = ''
-with open('README.md') as f:
-    readme = f.read()
+version = ""
+with open("./rustplus/__init__.py") as input_file:
+  for line in input_file.readlines():
+    if line.startswith("__version__"):
+      version = line.strip("__version__ =").strip().strip('"')
+      break
+
+readme = ""
+with open("README.md") as input_file:
+  readme = input_file.read()
 
 setup(
-      name='rustplus',
-      author='olijeffers0n',
-      author_email='pleaseUseMyDiscord@Please.com',
-      url='https://github.com/olijeffers0n/rustplus',
-      project_urls={
-        "Issue tracker": "https://github.com/olijeffers0n/rustplus/issues",
-      },
-      version="5.0.9",
-      include_package_data=True,
-      packages = find_packages(include=['rustplus', 'rustplus.*']),
-      license='MIT',
-      description='A python wrapper for the Rust Plus API',
-      long_description=readme,
-      long_description_content_type='text/markdown',
-      install_requires=[
-        "Pillow",
-        "ws4py",
-        "protobuf",
-        "asyncio"
-      ],
-      python_requires='>=3.7.0',
+  name='rustplus',
+  author='olijeffers0n',
+  author_email='pleaseUseMyDiscord@Please.com',
+  url='https://github.com/olijeffers0n/rustplus',
+  project_urls={
+    "Issue tracker": "https://github.com/olijeffers0n/rustplus/issues",
+  },
+  version=version,
+  include_package_data=True,
+  packages = find_packages(include=['rustplus', 'rustplus.*']),
+  license='MIT',
+  description='A python wrapper for the Rust Plus API',
+  long_description=readme,
+  long_description_content_type='text/markdown',
+  install_requires=[
+    "Pillow",
+    "ws4py",
+    "protobuf",
+    "asyncio"
+  ],
+  python_requires='>=3.7.0',
 )
