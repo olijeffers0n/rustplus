4379764f700a2a076414d603d08560461ad21e18
Ollie <69084614+olijeffers0n@users.noreply.github.com>
2022-03-11 23:46:25 +0000

    Update __init__.py

--- rustplus/__init__.py
+++ rustplus/__init__.py
@@ -10,5 +10,5 @@ from .utils import entity_type_to_string, convert_xy_to_grid
 
 __name__ = "rustplus"
 __author__ = "olijeffers0n"
-__version__ = "5.3.1"
+__version__ = "5.3.2"
 __support__ = "Discord: https://discord.gg/nQqJe8qvP8"
