58d4ea1f7d1d1ba7d274971cde2eb9f69c6b16d4
Ollie <69084614+olijeffers0n@users.noreply.github.com>
2022-04-10 15:20:13 +0100

    dont rely on expo_bundle_handler

--- rustplus/api/remote/expo_bundle_handler.py
+++ rustplus/api/remote/expo_bundle_handler.py
@@ -1,4 +1,5 @@
 import requests
+import logging
 import json
 from datetime import datetime
 
@@ -8,28 +9,32 @@ class MagicValueGrabber:
     @staticmethod
     def get_magic_value() -> int:
 
-        data = requests.get('https://exp.host/@facepunch/RustCompanion', headers={
+        try:
+            data = requests.get('https://exp.host/@facepunch/RustCompanion', headers={
 
-            'Exponent-SDK-Version': '40.0.0',   # These are the two lines that determine the version of data that you
-            'Exponent-Version': '0.0.21',       # receive as far as I can tell from my limited debugging. As of
-                                                # now (8th April 2022), this is the latest
+                'Exponent-SDK-Version': '40.0.0',   # These are the two lines that determine the version of data that
+                'Exponent-Version': '0.0.21',       # you receive as far as I can tell from my limited debugging. As of
+                                                    # now (8th April 2022), this is the latest
 
-            'Exponent-Platform': 'android',
-            'Accept': 'application/expo+json,application/json',
-            'Expo-Release-Channel': 'default',
-            'Expo-Api-Version': '1',
-            'Expo-Client-Environment': 'STANDALONE',
-            'Exponent-Accept-Signature': 'true',
-            'Expo-JSON-Error': 'true',
-            'Host': 'exp.host',
-            'User-Agent': 'okhttp/3.12.1'
-        })
+                'Exponent-Platform': 'android',
+                'Accept': 'application/expo+json,application/json',
+                'Expo-Release-Channel': 'default',
+                'Expo-Api-Version': '1',
+                'Expo-Client-Environment': 'STANDALONE',
+                'Exponent-Accept-Signature': 'true',
+                'Expo-JSON-Error': 'true',
+                'Host': 'exp.host',
+                'User-Agent': 'okhttp/3.12.1'
+            })
 
-        halves = str(json.loads(data.json()["manifestString"])["publishedTime"]).replace("Z", "").split("T")
+            halves = str(json.loads(data.json()["manifestString"])["publishedTime"]).replace("Z", "").split("T")
 
-        year, month, day = halves[0].split("-")
-        hour, minute, second = halves[1].split(":")
+            year, month, day = halves[0].split("-")
+            hour, minute, second = halves[1].split(":")
 
-        dt = datetime(int(year), int(month), int(day), int(hour), int(minute), 0)
+            dt = datetime(int(year), int(month), int(day), int(hour), int(minute), 0)
 
-        return int(((dt - datetime.utcfromtimestamp(0)).total_seconds() * 1000.0) + float(second) * 1000)
+            return int(((dt - datetime.utcfromtimestamp(0)).total_seconds() * 1000.0) + float(second) * 1000)
+        except Exception:
+            logging.getLogger("rustplus.py").warning("[Rustplus.py] Failed to get magic value from RustPlus Server")
+            return 1641359159846
