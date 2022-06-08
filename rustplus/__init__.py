b1a92f535cd6a141829187fa3fb8c442bc89a794
Ollie <69084614+olijeffers0n@users.noreply.github.com>
2022-06-08 13:32:11 +0100

    Bump Version and update protobuf

--- rustplus/__init__.py
+++ rustplus/__init__.py
@@ -12,5 +12,5 @@ from .utils import entity_type_to_string, convert_xy_to_grid
 
 __name__ = "rustplus"
 __author__ = "olijeffers0n"
-__version__ = "5.3.17"
+__version__ = "5.3.18"
 __support__ = "Discord: https://discord.gg/nQqJe8qvP8"
--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=[
         "websocket_client",
         "Pillow",
-        "protobuf>=3.18.1",
+        "protobuf>=3.20.0",
         "asyncio",
         "push_receiver",
         "http-ece",
