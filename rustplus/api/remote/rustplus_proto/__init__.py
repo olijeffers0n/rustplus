d2c25edd9f03ea09e8daf52246a26a64ea5170f0
Ollie <69084614+olijeffers0n@users.noreply.github.com>
2022-06-18 20:59:06 +0100

    Changes

--- rustplus/api/remote/rustplus_proto/__init__.py
+++ rustplus/api/remote/rustplus_proto/__init__.py
@@ -1,1 +1,4 @@
-from .rustplus_pb2 import *
+from .rustplus_pb2 import AppRequest, AppMessage, AppSendMessage, AppSetEntityValue, AppPromoteToLeader, AppResponse, \
+    AppBroadcast, AppEmpty, AppSuccess, AppError, AppInfo, AppTime, AppMap, AppTeamInfo, AppTeamChat, AppChatMessage, \
+    AppEntityInfo, AppEntityType, AppEntityPayload, AppFlag, AppMapMarkers, Vector4, AppMarker, AppMarkerType, \
+    AppTeamChanged, AppTeamMessage, AppEntityChanged
