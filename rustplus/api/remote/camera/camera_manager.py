4efe9709348a66d9476e340d33a66a32615767a2
Ollie <69084614+olijeffers0n@users.noreply.github.com>
2023-03-04 18:32:26 +0000

    Camera Close Logic

--- rustplus/api/remote/camera/camera_manager.py
+++ rustplus/api/remote/camera/camera_manager.py
@@ -1,5 +1,5 @@
 from typing import Iterable
-from ..rustplus_proto import AppCameraRays, AppCameraInfo, AppCameraInput, Vector2
+from ..rustplus_proto import AppCameraRays, AppCameraInfo, AppCameraInput, Vector2, AppEmpty
 from ...structures import Vector
 
 
@@ -10,6 +10,7 @@ class CameraManager:
         self._cam_id = cam_id
         self._last_packet: AppCameraRays = None
         self._cam_info_message: AppCameraInfo = cam_info_message
+        self._open = True
 
     def set_packet(self, packet):
         self._last_packet = packet
@@ -17,6 +18,9 @@ class CameraManager:
     def get_frame(self):
         if self._last_packet is None:
             return None
+
+        if not self._open:
+            raise Exception("Camera is closed")
 
         return self._last_packet
 
@@ -57,3 +61,14 @@ class CameraManager:
 
         await self.rust_socket.remote.send_message(app_request)
         self.rust_socket.remote.ignored_responses.append(app_request.seq)
+
+    async def exit_camera(self) -> None:
+        await self.rust_socket._handle_ratelimit()
+        app_request = self.rust_socket._generate_protobuf()
+        app_request.cameraUnsubscribe.CopyFrom(AppEmpty())
+
+        await self.rust_socket.remote.send_message(app_request)
+        self.rust_socket.remote.ignored_responses.append(app_request.seq)
+
+        self._open = False
+        self._last_packet = None
--- rustplus/api/remote/rustws.py
+++ rustplus/api/remote/rustws.py
@@ -205,7 +205,7 @@ class RustWebsocket(websocket.WebSocket):
 
         elif self.is_camera_broadcast(app_message):
             if self.remote.camera_manager is not None:
-                self.remote.camera_manager.set_packet(app_message)
+                self.remote.camera_manager.set_packet(app_message.broadcast.cameraRays)
 
         elif self.is_team_broadcast(app_message):
             # This means that the team of the current player has changed
