7804328410c6ff6e5611a70ae5b2eb68955ba68b
Ollie <69084614+olijeffers0n@users.noreply.github.com>
2023-05-28 20:17:13 +0100

    Cast to a string to avoid floats erroring betterproto

--- rustplus/api/rust_api.py
+++ rustplus/api/rust_api.py
@@ -88,7 +88,7 @@ class RustSocket(BaseRustSocket):
         await self._handle_ratelimit(2)
 
         app_send_message = AppSendMessage()
-        app_send_message.message = message
+        app_send_message.message = str(message)
 
         app_request = self._generate_protobuf()
         app_request.send_team_message = app_send_message
