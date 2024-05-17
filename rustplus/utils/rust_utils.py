9eb8f8c447bef723dd3dedc207286797437462b5
Shurik24 <Shurik24@night-watchers.com>
2024-05-17 15:36:18 +0200

    Fixed error with not found fonts

--- rustplus/utils/rust_utils.py
+++ rustplus/utils/rust_utils.py
@@ -1,9 +1,9 @@
 from importlib import resources
 from typing import Tuple
 from PIL import Image, ImageDraw, ImageFont
+from pathlib import Path
 import logging
 import string
-from pathlib import Path
 
 from ..api.remote.rustplus_proto import AppMessage
 from ..api.structures import RustTime
@@ -190,9 +190,9 @@ def _get_corrected_map_size(map_size):
     return map_size - remainder if remainder < 120 else map_size + offset
 
 
-def _is_outside_grid_system(x, y, map_size, offset = 0):
+def _is_outside_grid_system(x, y, map_size, offset=0):
     return (
-            x < -offset or x > (map_size + offset) or y < -offset or y > (map_size + offset)
+        x < -offset or x > (map_size + offset) or y < -offset or y > (map_size + offset)
     )
 
 
@@ -221,7 +221,7 @@ class HackyBackwardsCompatCoordClass:
 
 
 def convert_xy_to_grid(
-        coords: tuple, map_size: float, catch_out_of_bounds: bool = True
+    coords: tuple, map_size: float, catch_out_of_bounds: bool = True
 ) -> HackyBackwardsCompatCoordClass:
     corrected_map_size = _get_corrected_map_size(map_size)
 
@@ -232,13 +232,13 @@ def convert_xy_to_grid(
 
 
 def generate_grid(
-        map_size: int,
-        text_size: int = 20,
-        text_padding: int = 5,
-        text_font: Path = None,
-        color: str = 'black'
+    map_size: int,
+    text_size: int = 20,
+    text_padding: int = 5,
+    text_font: Path = None,
+    color: str = "black",
 ) -> Image.Image:
-    img = Image.new('RGBA', (map_size, map_size), (0, 0, 0, 0))
+    img = Image.new("RGBA", (map_size, map_size), (0, 0, 0, 0))
     d = ImageDraw.Draw(img)
 
     try:
@@ -247,7 +247,9 @@ def generate_grid(
         font = ImageFont.load_default(text_size)
 
     letters = list(string.ascii_uppercase)
-    letters.extend(a + b for a in string.ascii_uppercase for b in string.ascii_uppercase)
+    letters.extend(
+        a + b for a in string.ascii_uppercase for b in string.ascii_uppercase
+    )
 
     num_cells = int(map_size / GRID_DIAMETER)
 
@@ -255,10 +257,10 @@ def generate_grid(
         for j in range(num_cells):
             start = (i * GRID_DIAMETER, j * GRID_DIAMETER)
             end = ((i + 1) * GRID_DIAMETER, (j + 1) * GRID_DIAMETER)
-            d.rectangle([start, end], outline = color)
+            d.rectangle([start, end], outline=color)
 
             text = letters[i] + str(j)
             text_pos = (start[0] + text_padding, start[1] + text_padding)
-            d.text(text_pos, text, fill = color, font = font)
+            d.text(text_pos, text, fill=color, font=font)
 
     return img
