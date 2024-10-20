3cc0a36832d4a8ced446d38b49c5ef6503ace3b2
olijeffers0n <69084614+olijeffers0n@users.noreply.github.com>
2024-10-20 20:31:52 +0100

    Make the errors better

--- rustplus/structs/rust_error.py
+++ rustplus/structs/rust_error.py
@@ -1,7 +1,12 @@
+import logging
+from typing import Any
+
 from .serialization import Serializable
 
 
 class RustError(Serializable):
+
+    LOGGER = logging.getLogger("rustplus.py")
 
     def __init__(self, method: str, reason: str) -> None:
         self._method = method
@@ -17,3 +22,12 @@ class RustError(Serializable):
 
     def __str__(self) -> str:
         return f"Error Propagating from {self._method}: {self._reason}"
+
+    def __getattr__(self, attr_name: str) -> Any:
+        if attr_name in self.__dict__:
+            return self.__dict__[attr_name]
+
+        # This means we are gonna chuck the error in the user's face
+        self.LOGGER.error(
+            f"An Unhandled Error has occurred over the {self._method} method, reason: {self.reason}"
+        )
