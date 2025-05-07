9202afb77a79671cbfd60dde2fd0dcc991eed6c0
Ollie <69084614+olijeffers0n@users.noreply.github.com>
2025-05-07 22:26:56 +0100

    Update __init__.py

--- rustplus/__init__.py
+++ rustplus/__init__.py
@@ -14,5 +14,5 @@ from .structs import RustError, RustMarker, Vector
 
 __name__ = "rustplus"
 __author__ = "olijeffers0n"
-__version__ = "6.0.6"
+__version__ = "6.0.7"
 __support__ = "Discord: https://discord.gg/nQqJe8qvP8"
